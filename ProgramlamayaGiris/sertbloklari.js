6a60d88dfb456902c90955d20a3b28c201fb7247
aytajquseynova <you@example.com>
2023-04-10 19:59:07 +0400

    en boyuk ededin tapilmasi

---
+++ ProgramlamayaGiris/sertbloklari.js
@@ -0,0 +1,11 @@
+var number1=45
+var number2=54
+var number3=89
+//en boyuk ededi tap
+var enBoyuk=number2
+if(enBoyuk<number1){
+    console.log(enBoyuk=number1)
+}
+if(enBoyuk<number3){
+    console.log(enBoyuk=number3)
+}
