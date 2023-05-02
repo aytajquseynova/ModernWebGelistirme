f1e8b3b0a25e50360c3cbcf3e447178b3c168519
aytajquseynova <you@example.com>
2023-05-02 18:03:02 +0400

    js tasks

---
+++ JavascriptTemelleri/javascripttasks/task2/app.js
@@ -0,0 +1,3 @@
+let firstName = 'Alan';
+let lastName = 'Turing';
+console.log('"',"Sizin adınız",firstName,lastName,".Tanışlığımıza çox şadam!",'"')
---
+++ JavascriptTemelleri/javascripttasks/task5/app.js
@@ -0,0 +1,5 @@
+const fullName = 'Aytaj Huseynova';
+const onlyUpper = fullName.replace(/[a-z]/g, '');
+console.log(onlyUpper);
+//İstifadəçinin adını və soyadını boşluqlarla ayıraraq fullName dəyişəni yaradıb ona mənimsətin.
+//fullName dəyişəninin dəyərindən istifadə edərək istifadəçinin baş hərflərini konsola çıxaran proqram yazın.
