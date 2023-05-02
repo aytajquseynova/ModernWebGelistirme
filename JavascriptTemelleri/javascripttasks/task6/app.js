1b1cc61ee438ddcf0690bd0f839b606478260c73
aytajquseynova <you@example.com>
2023-05-02 18:07:49 +0400

    js task

---
+++ JavascriptTemelleri/javascripttasks/task6/app.js
@@ -0,0 +1,4 @@
+let fullName='Aytaj Huseynova';
+fullName='Huseynova Aytaj';
+console.log(fullName);
+//İstifadəçidən tam adı «AD SOYAD» formatında daxil etməyi xahiş edin. Tam adını konsola fərqli qaydada yəni «SOYAD AD» formatında çıxarın,
